f6303fe62afa47ce94227ed601c8753e475ef405
om <ombhutkar9@gmail.com>
2024-03-25 18:56:48 +0530

    2nd commit(latitude and longitude updated

--- assets/csv_to_json.py
+++ assets/csv_to_json.py
@@ -2,44 +2,23 @@ import csv
 import json
 
 def convert_csv_to_json(input_file, output_file):
-    connections = []
-    places = {}
-    place_id_counter = 1
+    routes = []
+    id_count = 1
     
     with open(input_file, 'r') as csvfile:
         reader = csv.DictReader(csvfile)
         
         for row in reader:
-            route_description = row["Route Description"]
-            route_split = route_description.split(" To ")
-            if len(route_split) != 2:
-                print(f"Skipping row with invalid Route Description: {route_description}")
-                continue
-            
-            source = route_split[0].strip()
-            destination = route_split[1].strip()
-            kilometer = row["Kilometer"]
-            
-            if source not in places:
-                places[source] = str(place_id_counter)
-                place_id_counter += 1
-            if destination not in places:
-                places[destination] = str(place_id_counter)
-                place_id_counter += 1
-            
-            connection = {
-                "source": places[source],
-                "destination": places[destination],
-                "weight": kilometer
+            route = {
+                "id": str(id_count),
+                "Route Description": row["Route Description"],
+                "Kilometer": row["Kilometer"]
             }
-            connections.append(connection)
-    
-    output_data = {
-        "places": [{"id": place_id, "place_name": place_name} for place_name, place_id in places.items()],
-        "connections": connections
-    }
+            routes.append(route)
+            id_count += 1
     
     with open(output_file, 'w') as jsonfile:
-        json.dump(output_data, jsonfile, indent=2)
+        json.dump(routes, jsonfile, indent=2)
 
-convert_csv_to_json("assets/bus_routes.csv", "assets/routes.json")
+# Example usage
+convert_csv_to_json("assets\bus_routes.csv", "assets\routes.json")
