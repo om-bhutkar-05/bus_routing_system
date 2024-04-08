211f6bcba3b6089e8dff4da899343a4411a563d2
om <ombhutkar9@gmail.com>
2024-04-08 15:00:54 +0530

    initial commit

--- main.cpp
+++ main.cpp
@@ -7,21 +7,21 @@ using namespace std;
 using json = nlohmann::json;
 
 int main() {
-    // Read data from routes2.json
+  
     ifstream file(("./assets/routes2.json"));
     json data;
     file >> data;
     file.close();
 
-    // Create a graph
+
     Graph graph;
 
-    // Add vertices from JSON data
+
     for (const auto& place : data["places"]) {
         graph.addVertex(place["id"], place["place_name"], stod(place["latitude"].get<string>()), stod(place["longitude"].get<string>()));
     }
 
-    // Add edges from JSON data
+
     for (const auto& connection : data["connections"]) {
         graph.addEdge(connection["source"], connection["destination"], stod(connection["weight"].get<string>()));
     }
@@ -54,21 +54,29 @@ int main() {
 
     // Find the shortest path
     list<string> path = graph.shortestPath(sourceId, destinationId);
-
-    // Output the distance of the shortest path
     if (!path.empty()) {
         double distance = graph.getVertex(destinationId)->distance;
         cout << "Distance of shortest path: " << distance << endl;
-
-        // Display the shortest path
         cout << "Shortest path from " << sourceName << " to " << destinationName << ":" << endl;
         for (const auto& vertex : path) {
             cout << graph.getVertex(vertex)->place_name << " -> ";
         }
         cout << endl;
-    } else {
-        cout << "No path found." << endl;
+         // Write latitude and longitude to file
+        ofstream outFile("latlng.txt");
+        if (outFile.is_open()) {
+            outFile << "lat:" << graph.getVertex(sourceId)->latitude << " ";
+            outFile << "long:" << graph.getVertex(sourceId)->longitude << " ";
+            outFile << "lat:" << graph.getVertex(destinationId)->latitude << " ";
+            outFile << "long:" << graph.getVertex(destinationId)->longitude << " ";
+            outFile.close();
+            cout << "Latitude and longitude written to latlng.txt successfully." << endl;
+        } else {
+            cerr << "Unable to open file." << endl;
+            return 1;
+        }
     }
+    
 
     return 0;
 }
